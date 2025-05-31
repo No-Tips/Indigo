faef898397e4f8efd4a3f1c551471be7b5ee7c67
igorsaux <igorsa.ux@outlook.com>
2025-05-31 12:03:53 +0300

    fix last warning

--- Content.Benchmarks/EntityQueryBenchmark.cs
+++ Content.Benchmarks/EntityQueryBenchmark.cs
@@ -9,6 +9,7 @@ using Content.Shared.Item;
 using Robust.Server.GameObjects;
 using Robust.Shared;
 using Robust.Shared.Analyzers;
+using Robust.Shared.EntitySerialization;
 using Robust.Shared.EntitySerialization.Systems;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
@@ -38,10 +39,12 @@ public class EntityQueryBenchmark
         _pair.Server.ResolveDependency<IRobustRandom>().SetSeed(42);
         _pair.Server.WaitPost(() =>
         {
-            var success = _entMan.System<MapLoaderSystem>().TryLoadMapWithId(_mapId, new(Map), out _, out _);
+            var success = _entMan.System<MapLoaderSystem>().TryLoadMapWithId(_mapId, new(Map), out _, out _, new DeserializationOptions()
+            {
+                InitializeMaps = true
+            });
             if (!success)
                 throw new Exception("Map load failed");
-            _pair.Server.MapMan.DoMapInitialize(_mapId);
         }).GetAwaiter().GetResult();
 
         _clothingQuery = _entMan.GetEntityQuery<ClothingComponent>();
